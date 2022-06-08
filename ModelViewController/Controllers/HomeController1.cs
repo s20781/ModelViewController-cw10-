55f824468a52d90bd60cb961198e62ecea02d8b1
Franciszek Baron <s20781@pjwstk.edu.pl>
2022-06-08 03:05:44 +0200

    Done

---
+++ ModelViewController/Controllers/HomeController1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ModelViewController.Controllers
+{
+    public class HomeController1 : Controller
+    {
+
+        [HttpGet("HelloWorld")]
+        public string Index(string name, int numTimes= 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Welcome")]
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+        
+    
+}
---
+++ ModelViewController/Data/ModelViewControllerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModelViewController.Models;
+
+namespace ModelViewController.Data
+{
+    public class ModelViewControllerContext : DbContext
+    {
+        public ModelViewControllerContext (DbContextOptions<ModelViewControllerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ModelViewController.Models.Movie> Movie { get; set; }
+    }
+}
